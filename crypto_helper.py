265f5cfbe4702c43aa83ca8973c561e340a649ca
Dennis_Vargas <dennis.vargas@wsu.edu>
2018-02-13 21:43:16 -0800

    Adjusts the docstrings in the following module, class, and gcd function.

--- crypto_helper.py
+++ crypto_helper.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 """ 
-a module which provides a number of crypto helper functions
+Provides a number of crypto helper functions
 """
 
 class CryptoHelper():
-  """[Class holding helper functions]
+  """[
+    Class holding helper functions]
   """
   def greatest_common_divisor(self,a,b):
-    """[Summary:
+    """[
+    Summary:
           Returns the largest number which divides 
           integers 'a' and 'b'. If 'b' is equal to 0 then 
-          'a' is returned. Arguments calculated as absolute value.]
+          'a' is returned. Arguments calculated as absolute value.
+          Implements Euclid's algorithm;
+          https://en.wikipedia.org/wiki/Euclidean_algorithm#Implementations]
     Arguments:
       a {[int]} -- [any integer positivie or negative]
       b {[int]} -- [any integer positive or negative]
